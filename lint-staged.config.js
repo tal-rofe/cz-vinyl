323bfdf1cbe1e6ab42436b51083cbc6d0ffabc2f
Tal Rofe <tal@vinyldepository.com>
2022-04-23 22:52:23 +0300

    refactor: 💡 add yaml extension to prettier
    add yaml extension to prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'{src,tests}/**/*.ts': 'eslint --fix',
-	'**/*.{ts,js,json}': 'prettier --write',
+	'**/*.{ts,js,json,yaml}': 'prettier --write',
 	'src/**/*.ts': 'jest -c jest.config.ts --bail --findRelatedTests',
 	'**/*': 'inflint -c inflint.config.js',
 };
