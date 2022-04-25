a8927ad9f0d9c169a90818dbf82681017fc57e73
Tal Rofe <tal@vinyldepository.com>
2022-04-25 23:23:25 +0300

    refactor: 💡 added gitattributes and TS inflint config file
    added gitattributes and TS inflint config file

--- inflint.config.ts
+++ inflint.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import { Config } from 'inflint';
+
+const inflintConfig: Config = {
 	rules: {
 		'{assets,scripts}/**/*': [2, 'kebab-case'],
 		'{src,tests}/**/*': [2, 'kebab-case.point'],
 	},
 };
+
+export default inflintConfig;
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'{src,tests}/**/*.ts': 'eslint --fix',
 	'**/*.{ts,js,json,yaml}': 'prettier --write',
 	'src/**/*.ts': 'jest -c jest.config.ts --bail --findRelatedTests',
-	'**/*': 'inflint -c inflint.config.js',
+	'**/*': 'inflint -c inflint.config.ts',
 };
